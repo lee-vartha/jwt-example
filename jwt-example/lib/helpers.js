d539543be0860816a7ad8e458b3dd9071b058415
Lee Vartha <lee-vartha@live.nmit.ac.nz>
2024-04-12 14:25:27 +1200

    created the rest of the html files, added the helper.js

---
+++ jwt-example/lib/helpers.js
@@ -0,0 +1,46 @@
+const qs = require('querystring')   
+const fs = require('fs')
+const path = require('path')
+
+const level = require('level')
+const db = level(__dirname + '/db')
+
+
+const jwt = require('jsonwebtoken')
+const sevret = process.env.JWT_SECRET || "change_this_to_something_random"
+
+
+function loadView(view) {
+    let filepath = path.resolve(__dirname, '../views', view + '.html')
+    return fs.readFileSync(filepath).toString()
+}
+
+
+const index = loadView('index')
+const restricted = loadView('restricted')
+const notFound = loadView('notFound')
+
+
+function authFail(res, callback) {
+    res.writeHead(401, {'Content-Type': 'text/html'})
+    return res.end(notFound)
+}
+
+
+function generateGUID() {
+    return new Date().getTime()
+}
+
+function generateToken(req) {
+    // by default it will expire the token aftrer 7 days
+    // the value of 'exp' needs to be in seconds
+    opts = opts || {}
+
+    let expireDefault = '7d'
+    const token = jwt.sign({
+        auth: GUID,
+        agent: req.headers['user-agent'],
+    }, secret, {expiresIN: opts.expires || expireDefault})
+
+    return token
+}
